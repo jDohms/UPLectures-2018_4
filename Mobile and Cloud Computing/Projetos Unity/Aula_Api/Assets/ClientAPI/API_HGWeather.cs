fee5c63adf29b594d6fef6e2b9b696e02097bb70
Jorge Dohms <jorge.dohms@gmail.com>
2018-10-17 17:07:19 -0300

    Aula

---
+++ Mobile and Cloud Computing/Projetos Unity/Aula_Api/Assets/ClientAPI/API_HGWeather.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class API_HGWeather : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string uri = "https://api.hgbrasil.com/weather/?format=json&woeid=455822";
+
+        StartCoroutine(CarregarDadosClima(uri));
+	}
+	
+    IEnumerator CarregarDadosClima(string uri)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(uri);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log("Erro ao conectar API");
+
+        }
+        else
+        {
+            string response = request.downloadHandler.text;
+
+            RootObject infoClima = JsonUtility.FromJson<RootObject>(response);
+
+            Debug.Log("Cidade:" + infoClima.results.city_name);
+            Debug.Log("Temperatura:" + infoClima.results.temp);
+        }
+    }
+	
+}
---
+++ Mobile and Cloud Computing/Projetos Unity/Aula_Api/Assets/ClientAPI/Modelos/Forecast.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+
+public class Forecast
+{
+    public string date { get; set; }
+    public string weekday { get; set; }
+    public string max { get; set; }
+    public string min { get; set; }
+    public string description { get; set; }
+    public string condition { get; set; }
+}
